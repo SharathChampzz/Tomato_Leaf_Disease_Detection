d887d4a2a329798997c2cbddec13b3b7babf9ab9
SharathChampzz <54370770+SharathChampzz@users.noreply.github.com>
2020-03-27 19:03:46 +0530

    Update test_network.py

--- test_network.py
+++ test_network.py
@@ -1,13 +1,3 @@
-# USAGE
-# python test_network.py --model leafDiease.model --image M:\pythonD\leaf_diseaseDatabse\plantVillage\Pepper__bell___Bacterial_spot\101.JPG   
-## python test_network.py --model leafDiease.model --image M:\pythonD\leaf_diseaseDatabse\plantVillage\Pepper__bell___healthy\100.JPG
-## python test_network.py --model leafDiease.model --image M:\pythonD\leaf_diseaseDatabse\plantVillage\Potato___Early_blight\10.JPG
-## python test_network.py --model M:\pythonD\15Days\AlexNetModel\AlexNetModel.hdf5 --image M:\pythonD\PlantVillageFullDataBase\Tomato__Tomato_mosaic_virus\101.JPG
-##python test_network.py --model M:\pythonD\15Days\kegal\cnn_model.pkl --image M:\pythonD\DandI\tomato\Tomato_Late_blight\1002.JPG
-
-
-
-
 # import the necessary packages
 import warnings
 warnings.simplefilter("ignore")
@@ -15,7 +5,6 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 import numpy as np
-##import argparse
 import imutils
 import cv2
 from os import listdir
@@ -24,47 +13,19 @@ ht=75
 wd=75
 className = []
 
-##totClass = 38
-
-##pathh = r"M:\pythonD\Delete"
+# Give the trained dataset path here it will just collect the labels 
+# or directly create a list with class names
 pathh = r"M:\pythonD\PlantVillageFullDataBase\Tomato"
 classNames = listdir(pathh)
 totClass = len(classNames)
 print(classNames)
 print(totClass)
-##print(MainFolder)
-##for classNam in MainFolder:
-##    className.append(classNam)
-##print(className)
-
-# construct the argument parse and parse the arguments
-##ap = argparse.ArgumentParser()
-##ap.add_argument("-m", "--model", required=True,
-##	help="path to trained model model")
-##ap.add_argument("-i", "--image", required=True,
-##	help="path to input image")
-##args = vars(ap.parse_args())
-##modell = "leafDisease.model"
-##pathhh = "M:/pythonD/DandI/tomato/Tomato_Late_blight"
-# load the image
 
 mdl = r"M:\pythonD\15Days\7cls\LeafDisease.h5"
-##im = r"M:\pythonD\Delete\Tomato___Bacterial_spot\01a3cf3f-94c1-44d5-8972-8c509d62558e___GCREC_Bact.Sp 3396.JPG"
-##im = r"M:\pythonD\Delete\Potato___Early_blight\065fc68f-88c9-4fc3-b0a6-a6f5e1072eaa___RS_Early.B 7174.JPG"
-##im = r"M:\pythonD\DataBase\PlantVillage\PlantVillage\validation\Potato___Early_blight\0a0744dc-8486-4fbb-a44b-4d63e6db6197___RS_Early.B 7575.JPG"
-##im = r"M:\pythonD\DataBase\PlantVillage\PlantVillage\train\Tomato___Bacterial_spot\00b7e89a-e129-4576-b51f-48923888bff9___GCREC_Bact.Sp 6202.JPG"
-##im = r"M:\pythonD\DataBase\PlantVillage\PlantVillage\train\Tomato___healthy\017a4026-813a-4983-887a-4052bb78c397___RS_HL 0218.JPG"
-##im = r"M:\pythonD\DataBase\PlantVillage\PlantVillage\train\Tomato___Target_Spot\01e0b8b1-e713-4c6d-973b-f7636280c58a___Com.G_TgS_FL 9816.JPG"
-##im = r"M:\pythonD\DataBase\PlantVillage\PlantVillage\validation\Tomato___Target_Spot\0a3b6099-c254-4bc3-8360-53a9f558a0c4___Com.G_TgS_FL 8259.JPG"
-##im = r"M:\pythonD\DataBase\PlantVillage\PlantVillage\validation\Potato___Early_blight\06ac6596-8d65-46dd-a343-a2209f3480e4___RS_Early.B 6921.JPG"
-##im = r"M:\pythonD\PlantVillageFullDataBase\Tomato\Tomato__Tomato_YellowLeaf__Curl_Virus\57.JPG"
-##im = r"M:\pythonD\PlantVillageFullDataBase\Tomato\Tomato_Early_blight\110.JPG"
-##im = r"M:\pythonD\PlantVillageFullDataBase\Tomato\Tomato_Late_blight\1001.JPG"
-##im = r"M:\pythonD\PlantVillageFullDataBase\Tomato\Tomato_Leaf_Mold\101.JPG"
-##im = r"M:\pythonD\PlantVillageFullDataBase\Tomato\Tomato_Septoria_leaf_spot\1005.JPG"
-##im = r"M:\pythonD\PlantVillageFullDataBase\Tomato\Tomato_Spider_mites_Two_spotted_spider_mite\15.JPG"
-##im = r"M:\pythonD\PlantVillageFullDataBase\Tomato\Tomato__Tomato_mosaic_virus\111.JPG"
+
+# Testing image path 
 im = r"M:\pythonD\tomTargetSPot.jpg"
+
 image = cv2.imread(im)
 orig = image.copy()
 
@@ -82,11 +43,11 @@ except Exception as e:
 # load the trained convolutional neural network
 print("[INFO] loading network...")
 model = load_model(mdl)
-##model = pickle.load(open('M:/pythonD/15Days/kegal/cnn_model.pkl', 'rb'))
+
 # classify the input image
-##classNames = ["zero", "one","two","three","four","five","six","seven","eight","nine","ozero", "oone","otwo","othree","ofour","ofive","osix","oseven","oeight","onine","tzero", "tone","ttwo","tthree","tfour","tfive","tsix","tseven","teight","tnine","tzero", "tone","ttwo","tthree","tfour","tfive","tsix","tseven"]
+# How wany classes do you have ?  Create those many variables here. 
 (zero, one,two, three,four,five,six,seven, eight,nine) = model.predict(image)[0]
-prob = [zero, one,two, three,four,five,six,seven, eight,nine]
+prob = [zero, one,two, three,four,five,six,seven, eight,nine] # also add here & these are not string variables
 
 maxProb = max(prob)
 maxIndex = prob.index(maxProb)
